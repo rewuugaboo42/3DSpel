e5fe890ec15f3c171b14fe268df2409761225bd6
rewuugaboo42 <188171707+rewuugaboo42@users.noreply.github.com>
2025-02-25 11:37:03 +0100

    Create GLFW window.

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,46 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+constexpr int SCREEN_WIDTH{ 800 };
+constexpr int SCREEN_HEIGHT{ 600 };
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
 int main()
 {
-	
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window{ glfwCreateWindow(800, 600, "Freakmon", NULL, NULL) };
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD\n";
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
 }
